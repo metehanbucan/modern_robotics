2959904658a8736d600dc8ecf0854831ac8e39d2
Metehan <metehanbucan@gmail.com>
2026-02-22 17:34:11 +0300

    add numerical inverse kinematics solver

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 from math_utils.kinematics import *
 from math_utils.jacobian import *
+from math_utils.ik import *
+
+M = np.eye(4)
+M[0:3, 3] = np.array([1,2,3])
 
 Slist = np.array([
     [0,0],
@@ -11,15 +15,11 @@ Slist = np.array([
     [0,0]
 ])
 
-ThetaList = np.array([np.pi/4 , np.pi/4])
+T_target = FkinSpace(M, Slist, np.array([np.pi/6 , np.pi/3]))
 
-Js = JacobianSpace(Slist, ThetaList)
+theta_guess = np.array([np.pi/4,np.pi])
 
-print(Js)
+theta_sol, success = IKinSpace(Slist, M, T_target, theta_guess, max_iter=500)
 
-thetadot = np.array([0.5, 0.5])
-
-V = Js @ thetadot
-
-print("end effector velocity:")
-print(V)
+print(theta_sol, success)
+print(np.array([np.pi/6 , np.pi/3]))
--- math_utils/ik.py
+++ math_utils/ik.py
@@ -1,4 +1,59 @@
 import numpy as np
-
+from math_utils.se3 import matrix_log3, so3_to_vec
+from math_utils.transformations import TransToRp
+from math_utils.jacobian import JacobianSpace
+from math_utils.kinematics import FkinSpace, vec_to_se3
 def matrix_log6(T):
+    R,p = TransToRp(T)
+    omgmat = matrix_log3(R)
+    omega = so3_to_vec(omgmat)
+
+    if np.linalg.norm(omega) < 1e-6:
+        se3mat = np.zeros((4,4))
+        se3mat[0:3,3] = p
+        return se3mat
     
+    theta = np.linalg.norm(omega)
+    omgmat_unit = omgmat / theta
+
+    G_inv = (np.eye(3) / theta - 0.5 * omgmat_unit + (1/theta - 0.5 / np.tan(theta / 2)) * (omgmat_unit @ omgmat_unit))
+
+    v = G_inv @ p
+
+    se3mat = np.zeros((4,4))
+    se3mat[0:3, 0:3] = omgmat
+    se3mat[0:3, 3] = v * theta
+
+    return se3mat
+
+def se3_to_vec(se3mat):
+    return np.array([
+        se3mat[2,1],
+        se3mat[0,2],
+        se3mat[1,0],
+        se3mat[0,3],
+        se3mat[1,3],
+        se3mat[2,3]
+    ])
+
+def IKinSpace(Slist, M, Tsd, thetalist0, eomg = 1e-3, ev= 1e-3, max_iter = 50):
+    thetalist = thetalist0.copy()
+    
+    for i in range(max_iter):
+        Tsb = FkinSpace(M, Slist, thetalist)
+        Tbd = np.linalg.inv(Tsb) @ Tsd
+
+        Vb = se3_to_vec(matrix_log6(Tbd))
+        err_omg = np.linalg.norm(Vb[0:3])
+        err_v = np.linalg.norm(Vb[3:6])
+
+        if err_omg < eomg and err_v < ev:
+            return thetalist, True
+        
+        J = JacobianSpace(Slist, thetalist)
+        thetalist += np.linalg.pinv(J) @ Vb
+
+    return thetalist, False
+
+
+
